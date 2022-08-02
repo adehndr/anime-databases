139b12f1707a9db87b6f153051725056d02c58aa
adehndr <56023937+adehndr@users.noreply.github.com>
2022-08-02 22:08:16 +0700

    Implementing mysql driver to the project. Implementing repository pattern.

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/adehndr/anime-databases/app"
+	"github.com/adehndr/anime-databases/repository"
+)
+
 func main() {
-	
+	dbMySql := app.OpenDatabaseConnection()
+	defer dbMySql.Close()
+
+	err := dbMySql.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	animeListRepository := repository.NewAnimeListRepository(dbMySql)
+	data, err := animeListRepository.FindAll(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(data)
 }
